3d9d16596ba48195b70535b3241438afb89a0677
videotutorialdev <12bedevnetwork@gmail.com>
2022-11-26 02:26:35 +0700

    add: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="container py-4">
+       <h1>Create Custom useForm Tutorial</h1>
+      <p>Welcome to create our own useForm ("rxid-useForm")</p>
+      <button className="btn btn-primary">
+        <em className="fas fa-plus me-1"/>
+        Add
+      </button>
+    </div>
+  );
+}
+
+export default App;
